94365f821c2714ce27fb291cb04421ffb469935d
Yazeed <Yazeedrq9@gmail.com>
2023-01-16 11:43:24 +0300

    lab is solved with bonus

--- main.py
+++ main.py
@@ -15,3 +15,14 @@ convertTuple=tuple(numberOflist)
 print(convertTuple)
 number1,number2,number3 = convertTuple
 print(number1,number2,number3)
+
+# Q Bonus : 
+
+lettersTofind= letters.index('b')
+print(lettersTofind)
+
+numberofCounts = numbers.count(2)
+print(numberofCounts)
+
+for indedx,i in enumerate(result):
+    print(indedx,i)
